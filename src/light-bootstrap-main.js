09ec7291be838f85c6f430497e5b9e47a4438fd9
alfin0224 <alfinmuhamad0224@gmail.com>
2023-09-07 11:56:28 +0700

    update descriptions

--- src/light-bootstrap-main.js
+++ src/light-bootstrap-main.js
@@ -13,9 +13,6 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './assets/sass/light-bootstrap-dashboard.scss'
 import './assets/css/demo.css'
 
-/**
- * This is the main Light Bootstrap Dashboard Vue plugin where dashboard related plugins are registerd.
- */
 export default {
   install (Vue) {
     Vue.use(GlobalComponents)
